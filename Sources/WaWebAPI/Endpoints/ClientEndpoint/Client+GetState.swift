eff03ddf8d953055571d5c7acbf43ee6f5358a6b
tierracero <vcantu01@tierracero.com>
2025-09-08 23:40:13 -0600

    minor fix

--- Sources/WaWebAPI/Endpoints/ClientEndpoint/Client+GetState.swift
+++ Sources/WaWebAPI/Endpoints/ClientEndpoint/Client+GetState.swift
@@ -11,25 +11,9 @@ import TCFundamentals
 import WaWebAPICore
 import Vapor
 
+ClientEndpoint.GetStateResponse: Content {}
+
 extension ClientEndpoint {
-    
-    public struct GetStateResponse: Content, Payloadable {
-        
-        /// conflict, connected, deprecatedVersion, opening, pairing, proxyblock, SMBTosBlock, timeout, TOSBlock, unlaunched, unpaired, unpairedIdle
-        public let state: WAState
-        
-        /// booting, loadingScreen, qr, authenticated, authFailure, ready, disconnected
-        public let status: InstanceStatus
-        
-        public init(
-            state: WAState,
-            status: InstanceStatus
-        ) {
-            self.state = state
-            self.status = status
-        }
-        
-    }
     
     /// Gets the current connection state for the client
     /// `POST` https://waweb.tierracero.co/api/v1/client/getState
