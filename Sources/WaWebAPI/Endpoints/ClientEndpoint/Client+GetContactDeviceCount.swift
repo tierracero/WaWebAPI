e43ef5080490a7f7fbd2d3c979bd20e9b041ef7a
tierracero <vcantu01@tierracero.com>
2025-12-17 09:04:13 -0600

    add GetContactDeviceCount GetContactLidAndPhone GetCustomerNoteRequest

---
+++ Sources/WaWebAPI/Endpoints/ClientEndpoint/Client+GetContactDeviceCount.swift
@@ -0,0 +1,34 @@
+//
+//  Client+GetContactById.swift
+//  
+//
+//  Created by Victor Cantu on 3/31/24.
+//
+
+import Foundation
+import TCFoundation
+import TCFundamentals
+import WaWebAPICore
+import Vapor
+
+extension ClientEndpoint.GetContactByIdRequest: Content {}
+
+extension ClientEndpoint {
+    
+    /// Gets the Contact's common groups with you. Returns empty array if you don't have any common group.
+    /// `POST` https://waweb.tierracero.co/api/v1/client/getCommonGroups
+    /// - Returns: Promise containing Array of WAWebJS.WhatsAppChatId
+    public func getContactById(
+        contactId: String
+    ) throws -> EventLoopFuture<WAResponsePayload<Contact>>{
+        do {
+            return try api.post(WAResponsePayload<Contact>.self, endpoint: .client("getContactById"), payload: GetContactByIdRequest(
+                contactId: contactId
+            ))
+        }
+        catch {
+            throw error
+        }
+    }
+    
+}
---
+++ Sources/WaWebAPI/Endpoints/ClientEndpoint/Client+GetContactLidAndPhone.swift
@@ -0,0 +1,1 @@
+Client+GetContactLidAndPhone.swift
